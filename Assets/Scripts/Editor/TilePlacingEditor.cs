3ed11398d2ca3f53d0ca0a2d87e1f6734f84b9d9
Adam Szkolny <adamszkolny@gmail.com>
2020-03-10 15:07:23 +0100

    Added tile placing and inspector extension

---
+++ Assets/Scripts/Editor/TilePlacingEditor.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(TilePlacing))]
+public class TilePlacingEditor : Editor
+{
+    SerializedProperty x;
+    SerializedProperty y;
+    SerializedProperty z;
+    SerializedProperty tileRadius;
+
+    void OnEnable()
+    {
+        x = serializedObject.FindProperty("x");
+        y = serializedObject.FindProperty("y");
+        z = serializedObject.FindProperty("z");
+        tileRadius = serializedObject.FindProperty("tileRadius");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+        EditorGUILayout.PropertyField(x);
+        EditorGUILayout.PropertyField(y);
+        EditorGUILayout.PropertyField(z);
+        EditorGUILayout.PropertyField(tileRadius);
+        if (tileRadius.floatValue < 0)
+        {
+            tileRadius.floatValue = 0;
+        }
+
+        serializedObject.ApplyModifiedProperties();
+        if (GUILayout.Button("Snap"))
+        {
+            Debug.Log("Snapping");
+            if (x.intValue + y.intValue + z.intValue == 0)
+            {
+                TilePlacing tilePlacing = (TilePlacing)target;
+                tilePlacing.PlaceTile();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/TilePlacing.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TilePlacing : MonoBehaviour
+{
+    public int x, y, z;
+    
+    public float tileRadius;
+    static float sqrt3 = Mathf.Sqrt(3f);
+
+    void OnValidate()
+    {
+
+    }
+
+    /// <summary>
+    /// Each time the sum of x, y and z coordinates equals 0 the tile is placed in corresponding place
+    /// </summary>
+    public void PlaceTile()
+    {
+        Vector3 NEVector = (new Vector3(sqrt3, 0, 1)).normalized * tileRadius * sqrt3;
+        Vector3 SEVector = (new Vector3(sqrt3, 0, -1)).normalized * tileRadius * sqrt3;
+
+        if (x + y + z == 0)
+        {
+            Debug.Log("PlaceTile");
+
+            Vector3 position = x * NEVector + y * SEVector;
+            transform.position = position;
+        }
+    }
+}
